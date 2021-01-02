a3519ffd279e3f850a6951b3a63f5b20af2e569d
BonnyWang <bonny.y.gardy@gmail.com>
2021-01-02 11:06:57 -0500

    Init(Tilemap Movement)

---
+++ Assets/playerControl.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerControl : MonoBehaviour{
+    Vector3 destPosition;
+    Vector3 tempPosition;
+    [SerializeField] float step;
+    GridLayout gridLayout;
+    // Start is called before the first frame update
+    void Start(){
+        destPosition = transform.position;
+        tempPosition = transform.position;
+        gridLayout = transform.parent.GetComponentInParent<GridLayout>();
+       
+    }
+
+    // Update is called once per frame
+    void Update(){
+        
+        if(Input.GetButtonDown("Click")){
+            destPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+        
+        tempPosition = Vector2.MoveTowards(tempPosition,destPosition, step);
+
+        // convert continous to discrete, if same cell then no movement
+        Vector3Int cellPosition = gridLayout.WorldToCell(tempPosition);
+        transform.position = gridLayout.CellToWorld(cellPosition);
+    
+    }
+
+   
+}
